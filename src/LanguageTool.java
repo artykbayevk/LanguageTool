25bbff2ae22d512491d4f0b5ba359cde4bb67031
kamalsdu <Aiser123>
2018-11-06 16:10:25 +0600

    struct of DFA

--- src/LanguageTool.java
+++ src/LanguageTool.java
@@ -1,5 +1,6 @@
 import util.Config;
 import java.io.File;
+import java.util.Scanner;
 import java.io.IOException;
 
 
@@ -10,11 +11,18 @@ public class LanguageTool {
         File baseDirectory  = new File(config.abs_path);
         File file = new File(baseDirectory, "structure_DFA.txt");
 
-
         if(file.exists()){
             System.out.println("File here");
         }else{
             System.out.println("Not here ");
         }
+
+
+        System.out.println("Language Tool Interface:\n");
+        Scanner reader = new Scanner(System.in);
+        System.out.println("1 - Load DFA");
+        System.out.println("2 - Load NFA");
+        System.out.println("3 - Load RegExp\n");
+        int n = reader.nextInt();
     }
 }

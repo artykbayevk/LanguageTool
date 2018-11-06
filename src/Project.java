9530e708d5a499352ce6d9581f5b137bc3f52303
kamalsdu <Aiser123>
2018-11-06 19:28:18 +0600

    Created main class

---
+++ src/Project.java
@@ -0,0 +1,21 @@
+import util.Config;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Project {
+    public static void main(String[] args) {
+        Config config = new Config();
+        File baseDirectory  = new File(config.abs_path);
+        File file = new File(baseDirectory, "structure_DFA.txt");
+
+        if(file.exists()){
+            System.out.println("File here");
+        }else{
+            System.out.println("Not here ");
+        }
+
+
+        LanguageTool tool = new LanguageTool(file.toString(), "DFA", baseDirectory.toString());
+    }
+}

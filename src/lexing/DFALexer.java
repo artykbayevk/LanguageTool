c1e9ca0dbaf842c55cc46cc22453d5da3cab151f
kamalsdu <Aiser123>
2018-11-08 16:36:46 +0600

    Lexer Structure changed

--- src/lexing/DFALexer.java
+++ src/lexing/DFALexer.java
@@ -20,11 +20,18 @@ public class DFALexer extends Lexer {
             do {
                 ch = reader.read();
                 if(ch == -1){
-                    System.out.println(line + line.length());
-                    break;
+
                 }
                 if((char)ch == '\n'){
                     String[] splited = line.split("\\s+");
+                    if(splited.length != 1){
+                        System.out.println("Lenght of splitted + "+splited.length);
+                        for (int i = 0; i < splited.length; i++) {
+                            System.out.print(splited[i]);
+                        }
+                        System.out.println('\n');
+                    }
+
                     line="";
                 }else{
                     line+=(char)ch;

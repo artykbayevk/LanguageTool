91b318fe04df8cccb0129220530a21c350ac15a3
kamalsdu <Aiser123>
2018-11-08 19:35:42 +0600

    Fucking lexer

--- src/lexing/DFALexer.java
+++ src/lexing/DFALexer.java
@@ -4,6 +4,7 @@ import util.Token;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class DFALexer extends Lexer {
@@ -15,45 +16,27 @@ public class DFALexer extends Lexer {
     public List<Token> getTokens() throws IOException {
         try{
             BufferedReader reader = new BufferedReader(new FileReader(file));
-            int ch,col=0,row=0;
-            boolean break_stm = false;
-            String line = "";
-            do {
-                ch = reader.read();
-
-                //if our file is empty, end we just write this is empty file
-                if(ch == -1 && line.length()==0 && col == 0 && row == 0){
-                    System.out.println("File is empty");
-                }else if(ch == -1 && line.length()!=0){ // or we take last line and split it
-                    String[] splited = line.split("\\s+");
-                    LineToToken(splited,break_stm);
+            String line;
+            boolean valid = false;
+            while ((line = reader.readLine())!= null) {
+                if (!line.trim().equals("")) {
+                    valid = true;
+                    LineToToken(line.replaceAll("\\s", ""));
                 }
+            }
 
-                if((char)ch == '\n'){
-                    String[] splited = line.split("\\s+");
-                    //not space
-                    if (splited.length == 1 && splited[0].equals("-")) break_stm = true;
+            if(!valid){
+                System.out.println("File is empty");
+            }
 
-
-                    if(splited.length != 1){
-                        LineToToken(splited,break_stm);
-                    }
-                    line="";
-                    row++;
-                }else{
-                    line+=(char)ch;
-                }
-            }while (ch != -1);
         }catch (IOException e){
-            System.out.println("Problem in reading file!");
+            throw e;
         }
         return LexerCharList;
     }
 
 
-    public void LineToToken(String[] lineArr,boolean break_stm){
-        String line = "";
-        for (int i = 0; i < lineArr.length; i++)line+=lineArr[i];
-        System.out.println(line + ' ' + lineArr.length + ' ' + break_stm);
+    private void LineToToken(String line){
+        System.out.println(line);
     }
 }
